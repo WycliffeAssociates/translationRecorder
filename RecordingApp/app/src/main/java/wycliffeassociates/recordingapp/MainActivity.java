7849c7545d563e9801de3df3d4387fd535cba2df
Joseph Sarabia <lordparrot@live.com>
2015-07-14 11:04:50 -0400

    Adds the ability to save a recorded file
    Prompts the user for a name to save the recorded file.

--- RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/MainActivity.java
+++ RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/MainActivity.java
@@ -1,14 +1,23 @@
 package wycliffeassociates.recordingapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
     private String recordedFilename = null;
     private WavRecorder recorder = null;
+    final Context context = this;
+    private String outputName = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,7 @@ public class MainActivity extends Activity {
         findViewById(R.id.btnRecord).setOnClickListener(btnClick);
         findViewById(R.id.btnStop).setOnClickListener(btnClick);
         findViewById(R.id.btnPlay).setOnClickListener(btnClick);
+        findViewById(R.id.btnSave).setOnClickListener(btnClick);
     }
 
     private void enableButton(int id,boolean isEnable){
@@ -33,19 +43,65 @@ public class MainActivity extends Activity {
         enableButton(R.id.btnRecord, !isRecording);
         enableButton(R.id.btnStop, isRecording);
         enableButton(R.id.btnPlay, !isRecording);
+        enableButton(R.id.btnSave, true);
     }
 
+    private void saveRecording(){
+        try {
+            getSaveName(context);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean getSaveName(Context c){
+        final EditText toSave = new EditText(c);
+        toSave.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        //prepare the dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setTitle("Save as");
+        builder.setView(toSave);
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                setName(toSave.getText().toString());
+                //SAVE FILE HERE
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        return true;
+    }
+
+    public void setName(String newName){
+        outputName = newName;
+        recordedFilename= recorder.saveFile(outputName);
+    }
+
+    public String getName(){return outputName;}
+
+
+
     private void startRecording(){
+        recorder = null;
         Toast.makeText(getApplicationContext(), "Starting Recording", Toast.LENGTH_LONG).show();
         recorder = new WavRecorder();
         recorder.record();
     }
     private void stopRecording(){
         recorder.stop();
-        recordedFilename= recorder.saveFile("test");
         Toast.makeText(getApplicationContext(), "Stopping Recording", Toast.LENGTH_LONG).show();
-        recorder = null;
-
     }
     private void playRecording(){
         Toast.makeText(getApplicationContext(), "Playing Audio", Toast.LENGTH_LONG).show();
@@ -71,6 +127,10 @@ public class MainActivity extends Activity {
                 case R.id.btnPlay:{
                     enableButtons(false);
                     playRecording();
+                }
+                case R.id.btnSave:{
+                    saveRecording();
+                    break;
                 }
             }
         }
