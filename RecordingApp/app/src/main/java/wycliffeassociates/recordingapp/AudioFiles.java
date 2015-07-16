e52b8204276677b8c540b0c36022d0b884e764fb
WeAreButler <sw92821@aol.com>
2015-07-16 15:23:29 -0700

    v.5 Unimplemented sorting
    Sorting by FileName and LastModified works -- yet not implemented /
    refractor

--- RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/AudioFiles.java
+++ RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/AudioFiles.java
@@ -65,59 +65,12 @@ public class AudioFiles extends Activity {
         }
 
 
-
-        //ArrayList<String> stringList = new ArrayList<String>();
-        //stringList = sortString(audioNameList, true);
-        //new ArrayList<String>();
-
-        //for(int e = 0; e < stringList.size(); e++){
-        //    System.out.println(stringList.get(e));
-        //}
-
-
-
-
         //add files to adapter to display to the user
-        ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, audioNameList);
+        ArrayAdapter<Date> arrayAdapter =
+                new ArrayAdapter<Date>(this,android.R.layout.simple_list_item_1, sortDate(dateList,true));
         audioList.setAdapter(arrayAdapter);
 
 
-        /*Boolean flag = true;
-        /*System.out.println("---------------");
-        ArrayList<Date> dateList2 = new ArrayList<Date>();
-        ArrayList<Date> dateList3 = new ArrayList<Date>();
-        for(int u= 0; u < dateList.size()-1; u++){
-            dateList2.add(dateList.get(u));
-            dateList3.add(dateList.get(u));
-        }
-
-        ArrayList<String> audioList2 = new ArrayList<String>();
-        ArrayList<String> audioList3 = new ArrayList<String>();
-        for(int u= 0; u < audioNameList.size()-1; u++){
-            audioList2.add(audioNameList.get(u));
-            audioList3.add(audioNameList.get(u));
-        }
-
-        ArrayList<Date> test = sortDate(dateList2, false);
-        for(int l = 0; l < test.size(); l++){
-            System.out.println(test.get(l));
-        }
-        System.out.println("---------------");
-        ArrayList<Date> test3 = sortDate(dateList3, true);
-        for(int l = 0; l < test3.size(); l++){
-            System.out.println(test3.get(l));
-        }
-        System.out.println("---------------");
-        ArrayList<String> test2 = sortString(audioList2, flag);
-        for(int l = 0; l < test2.size(); l++){
-            System.out.println(test2.get(l));
-        }
-        System.out.println("---------------");
-        ArrayList<String> test4 = sortString(audioList3, false);
-        for(int l = 0; l < test4.size(); l++){
-            System.out.println(test4.get(l));
-        }*/
         //on item list click -- play
         audioList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             // argument position gives the index of item which is clicked
@@ -142,6 +95,7 @@ public class AudioFiles extends Activity {
         do{
             size = dList.size()-1;
             cmp = dList.get(size);
+            val = size;
 
             //compare with other items
             for(int x = 0; x < size; x++){
@@ -176,6 +130,7 @@ public class AudioFiles extends Activity {
     //flag == false : sort by Z-A
     private ArrayList<String> sortString (ArrayList<String> nList, Boolean flag){
         ArrayList<String> outputList = new ArrayList<String>();
+
         String cmp = "";
         int val = 0;
         int size = nList.size() - 1;
@@ -183,11 +138,12 @@ public class AudioFiles extends Activity {
         //as long as there are items
         do{
             size = nList.size()-1;
-            cmp = nList.get(size);
+            cmp = nList.get(size).toLowerCase();
+            val = size;
 
             //compare with other items
             for(int x = 0; x < size; x++){
-                if (cmp.compareTo(nList.get(x)) < 0) {
+                if (cmp.compareTo(nList.get(x).toLowerCase()) < 0) {
                     if(flag){
                         //A-Z
                     }else{
@@ -205,6 +161,7 @@ public class AudioFiles extends Activity {
                     }
                 }
             }
+            System.out.println(val);
             nList.remove(val);
             outputList.add(cmp);
 
