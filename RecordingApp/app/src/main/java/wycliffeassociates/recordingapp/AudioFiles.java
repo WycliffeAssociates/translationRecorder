5ce231a7667912b87834215938a5eb1ba5e93b45
a-gundy <a-gundy@bethel.edu>
2015-07-28 14:51:31 -0400

    Removed Export Files
    Removed export files class & xml and moved everything to AudioFiles.java

--- RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/AudioFiles.java
+++ RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/AudioFiles.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
 import android.view.Menu;
@@ -13,7 +14,13 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.net.Uri;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -59,6 +66,21 @@ public class AudioFiles extends Activity {
     Hashtable<Date, String> audioHash = new Hashtable<Date, String>();
 
     static String directory = "";
+
+    /**
+     * the current filepath being exported
+     */
+    private String thisPath;
+
+    /**
+     * the total number of files being exported
+     */
+    private int totalFiles = 0;
+
+    /**
+     * the number of the current file bein exported (corresponds with allMoving)
+     */
+    private int fileNum =0;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -163,10 +185,13 @@ public class AudioFiles extends Activity {
                             AudioFiles.AudioExport(items2[i].getName(), adapter.checkBoxState[i]);
                         }
                     }
-                    Intent intent = new Intent(v.getContext(), ExportFiles.class);
+//                    Intent intent = new Intent(v.getContext(), ExportFiles.class);
                     if (exportList.size() > 0) {
-                        intent.putExtra("exportList", exportList);
-                        startActivityForResult(intent, 0);
+//                        intent.putExtra("exportList", exportList);
+//                        startActivityForResult(intent, 0);
+                        totalFiles = exportList.size();
+                        thisPath = exportList.get(0);
+                        createFile("audio/*", getNameFromPath(thisPath));
                     } else {
                         Toast.makeText(AudioFiles.this, "Failed", Toast.LENGTH_SHORT).show();
                     }
@@ -377,6 +402,95 @@ public class AudioFiles extends Activity {
             Arrays.fill(adapter.checkBoxState, Boolean.FALSE);
         }
         finish();
+    }
+
+    /**
+     * Iterates the file number that is being looked a
+     * @return Returns true if iteration worked, false if the end has been reached
+     */
+    public boolean iteratePath(){
+        if(fileNum + 1 < totalFiles) {
+            fileNum++;
+            return true;
+        }
+        if(fileNum + 1 == totalFiles){
+            fileNum++;
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * A method to extract filename from the path
+     * @param path The paths to the files
+     * @return The simple filename of the file
+     */
+    public String getNameFromPath(String path){
+        String[] temp = path.split("/");
+        return temp[temp.length-1];
+    }
+
+    /**
+     * Copies a file from a path to a uri
+     * @param destUri The desination of the file
+     * @param path The original path to the file
+     */
+    public void savefile(Uri destUri, String path)
+    {
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        try {
+            String sourceFilename = path;
+            ParcelFileDescriptor destinationFilename = getContentResolver().
+                    openFileDescriptor(destUri, "w");
+            bis = new BufferedInputStream(new FileInputStream(sourceFilename));
+            bos = new BufferedOutputStream(new FileOutputStream(destinationFilename.getFileDescriptor()));
+            byte[] buf = new byte[1024];
+            bis.read(buf);
+            do {
+                bos.write(buf);
+            } while(bis.read(buf) != -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bis != null) bis.close();
+                if (bos != null) bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            iteratePath();
+            if(fileNum < totalFiles) {
+                thisPath = exportList.get(fileNum);
+                createFile("audio/*", getNameFromPath(thisPath));
+            }
+        }
+
+    }
+
+    /**
+     * Creates a file in folder selected by user
+     * @param mimeType Typically going to be "audio/*" for this app
+     * @param fileName The name of the file selected.
+     */
+    private void createFile(String mimeType, String fileName) {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT );
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType(mimeType);
+        intent.putExtra(Intent.EXTRA_TITLE, fileName);
+        this.startActivityForResult(intent, 43);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+        Uri currentUri = null;
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 43) {
+                currentUri = resultData.getData();
+                savefile(currentUri, exportList.get(fileNum));
+
+            }
+        }
     }
 }
 
