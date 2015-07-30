1d44424b3e255895a34283d28e570f799e905644
a-gundy <a-gundy@bethel.edu>
2015-07-30 10:04:29 -0400

    default name changes

--- RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/DefaultName.java
+++ RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/DefaultName.java
@@ -79,18 +79,7 @@ public class DefaultName extends Activity
 
 
     }
-/*
-    public void setUpLanguageSpinner(){
-        Spinner spinner = (Spinner) findViewById(R.id.langCode);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.planets_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-    }
-*/
+
     /**
      * Getter for the unique identifier of the device
      * @return the unique identifier of the device
