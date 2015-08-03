f57e8d35a1b55d03b58758dd254b487a759a42d7
etagoe <emmanuel_tagoe@rocketmail.com>
2015-08-03 16:02:38 -0400

    ftp upload
    uploads multiple files to a remote ftp server
    displays progress of upload

--- RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/FTPActivity.java
+++ RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/FTPActivity.java
@@ -1,6 +1,8 @@
 package wycliffeassociates.recordingapp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,9 +12,12 @@ import android.widget.EditText;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.io.CopyStreamAdapter;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 
 public class FTPActivity extends Activity {
@@ -24,15 +29,21 @@ public class FTPActivity extends Activity {
     private EditText Directory;
     private Button Ok;
     private Button Cancel;
+    CopyStreamAdapter streamListener;
 
-    String filepath, server, password, direc, user;
+    PreferencesManager pref;
+
+    String filepath, server, password, direc, user, destinationfilename;
     int port;
     File uploadFile;
+    int increment = 0;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ftpdialog);
+        pref = new PreferencesManager(this);
 
         Ok = (Button)findViewById(R.id.btnOK);
 
@@ -50,16 +61,11 @@ public class FTPActivity extends Activity {
                 password = Password.getText().toString();
                 Directory = (EditText) findViewById(R.id.fileDirectory);
                 direc = Directory.getText().toString();
-                filepath = AudioFiles.exportList.get(0);
-                uploadFile = new File(filepath);
-
-                new UploadFile().execute(filepath, server, user, password);
-
+                new UploadFile().execute();
             }
         });
 
         Cancel = (Button)findViewById(R.id.btnCANCEL);
-
         Cancel.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -67,20 +73,42 @@ public class FTPActivity extends Activity {
             }
         });
 
-
     }
 
     private class UploadFile extends AsyncTask<String, Integer, Boolean> {
+        public ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(FTPActivity.this);
+            progressDialog.setMessage("File(s) is Uploading to Server");
+            progressDialog.setIndeterminate(false);
+            progressDialog.setTitle("UPLOAD FILE(S)");
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progressDialog.setProgress(0);
+            progressDialog.setMax(AudioFiles.exportList.size());
+            progressDialog.show();
+        }
 
         @Override
         protected Boolean doInBackground(String... params) {
             FTPClient client = new FTPClient();
+            boolean result = false;
             try {
                 client.setUseEPSVwithIPv4(true);
-                client.connect(server, port);
-                client.login(user, password);
-                client.setFileType(FTP.BINARY_FILE_TYPE);
-                boolean result = client.storeFile("naruto.wav",new FileInputStream(uploadFile));
+                client.connect(server, port); // connect to server
+                client.login(user, password); // login to server
+                client.setFileType(FTP.BINARY_FILE_TYPE); // set file type
+                for (int i = 0; i < AudioFiles.exportList.size(); i ++) {
+                    filepath = AudioFiles.exportList.get(i);
+                    uploadFile = new File(filepath);
+                    destinationfilename = filepath.replace((String) pref.getPreferences("fileDirectory") + "/", "");
+                    result = client.storeFile(destinationfilename, new FileInputStream(uploadFile)); // store file on server
+                    publishProgress(i+1);
+                    System.out.println("Status Value-->" + result);
+                }
+                client.logout(); // logout of the server
+                client.disconnect(); // disconnect from the server
                 return result;
             } catch (Exception e) {
                 Log.d("FTP", e.toString());
@@ -89,11 +117,22 @@ public class FTPActivity extends Activity {
         }
 
         @Override
+        protected void onProgressUpdate(Integer... progress) {
+            super.onProgressUpdate(progress);
+            progressDialog.setProgress(progress[0]);
+        }
+
+        @Override
         protected void onPostExecute(Boolean sucess) {
-            if (sucess)
+            progressDialog.setProgress(AudioFiles.exportList.size());
+            progressDialog.dismiss();
+            if (sucess) {
                 System.out.println("Success");
-            else
+            }
+            else {
                 System.out.println("Error");
+            }
+            finish();
         }
     }
 }
