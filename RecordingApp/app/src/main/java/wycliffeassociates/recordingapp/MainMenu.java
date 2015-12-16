0cdc25f80ca06e4886cc9256df0f7f8d7aa8c51c
Joe <jsarabia1247@gmail.com>
2015-12-16 10:02:27 -0500

    Initial upload of Logger
    Implements the Logger from TS, does not currently have github
    functionality

--- RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/MainMenu.java
+++ RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/MainMenu.java
@@ -3,13 +3,16 @@ package wycliffeassociates.recordingapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ImageButton;
 
 import java.io.File;
 
+import Reporting.Logger;
 import wycliffeassociates.recordingapp.FilesPage.AudioFiles;
 import wycliffeassociates.recordingapp.Recording.RecordingScreen;
 import wycliffeassociates.recordingapp.SettingsPage.Settings;
@@ -19,17 +22,28 @@ public class MainMenu extends Activity{
     private ImageButton btnRecord;
     private ImageButton btnFiles;
     private ImageButton btnSettings;
+    public static final String KEY_PREF_LOGGING_LEVEL = "logging_level";
+    public static final String PREF_DEFAULT_LOGGING_LEVEL = "1";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // configure logger
+        int minLogLevel = Integer.parseInt(getUserPreferences().getString(KEY_PREF_LOGGING_LEVEL, PREF_DEFAULT_LOGGING_LEVEL));
+        configureLogger(minLogLevel);
+        Logger.i(MainMenu.class.toString(), "Initialized logger");
 
         System.out.println("internal files dir is " + getApplicationContext().getFilesDir());
         System.out.println("External files dir is " + Environment.getExternalStoragePublicDirectory("TranslationRecorder"));
 
 
         File visDir = new File(Environment.getExternalStoragePublicDirectory("TranslationRecorder"), "/Visualization");
-        System.out.println("Result of making vis directory "+visDir.mkdir());
+        System.out.println("Result of making vis directory " + visDir.mkdir());
+
+
+
         AudioInfo.pathToVisFile = visDir.getAbsolutePath() + "/";
         btnRecord = (ImageButton) findViewById(R.id.new_record);
         btnRecord.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +77,16 @@ public class MainMenu extends Activity{
         });
     }
 
+    public void configureLogger(int minLogLevel) {
+        Logger.configure(new File(getExternalCacheDir(), "log.txt"), Logger.Level.getLevel(minLogLevel));
+    }
 
-
+    /**
+     * Returns an instance of the user preferences.
+     * This is just the default shared preferences
+     * @return
+     */
+    public SharedPreferences getUserPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(this);
+    }
 }
