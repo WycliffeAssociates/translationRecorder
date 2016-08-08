2e459051292acd644d7e8e5a04928ca736cd0d4d
Joe <jsarabia1247@gmail.com>
2016-08-08 16:51:52 -0400

    added a table to store info about rating and check level on takes

--- RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/ProjectManager/ProjectContract.java
+++ RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/ProjectManager/ProjectContract.java
@@ -1,6 +1,21 @@
 package wycliffeassociates.recordingapp.ProjectManager;
 
+import android.media.Rating;
 import android.provider.BaseColumns;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.TABLE_PROJECT;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.TABLE_TAKES;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_TARGET_LANG;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_SLUG;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_SOURCE;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_PROJECT;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_MODE;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_SOURCE_LANG;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_CHAPTER;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_START_VS;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_END_VS;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_RATING;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_CHECKING;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_TAKE_NUM;
 
 /**
  * Created by sarabiaj on 5/19/2016.
@@ -21,11 +36,20 @@ public final class ProjectContract {
         public static final String COLUMN_MODE = "key_mode";
         public static final String COLUMN_CONTRIBUTORS = "key_contributors";
         public static final String COLUMN_SOURCE_AUDIO_PATH = "key_source_audio_path";
+        public static final String TABLE_TAKES = "takes";
+        public static final String COLUMN_CHAPTER = "key_chapter";
+        public static final String COLUMN_START_VS = "key_start_vs";
+        public static final String COLUMN_END_VS = "key_end_vs";
+        public static final String COLUMN_RATING = "key_rating";
+        public static final String COLUMN_CHECKING = "key_checking";
+        public static final String COLUMN_TAKE_NUM = "key_take_num";
     }
 
     public static final String TEXT = " TEXT";
+    public static final String INTEGER = " INTEGER";
     public static final String COMMA = ",";
     public static final String TEXTCOMMA = " TEXT,";
+    public static final String INTCOMMA = " INTEGER,";
 
     public static final String CREATE_PROFILE_TABLE = "CREATE TABLE " + ProjectEntry.TABLE_PROJECT + " ("
             + ProjectEntry._ID + " INTEGER PRIMARY KEY,"
@@ -40,6 +64,20 @@ public final class ProjectContract {
             + ProjectEntry.COLUMN_SOURCE_AUDIO_PATH + TEXT
             + " )";
 
+    public static final String CREATE_TAKES_TABLE = "CREATE TABLE " + TABLE_TAKES + " ("
+            + ProjectEntry._ID + " INTEGER PRIMARY KEY,"
+            + COLUMN_TARGET_LANG + TEXTCOMMA
+            + COLUMN_SLUG + TEXTCOMMA
+            + COLUMN_SOURCE + TEXTCOMMA
+            + COLUMN_CHAPTER + INTCOMMA
+            + COLUMN_START_VS + INTCOMMA
+            + COLUMN_END_VS + INTCOMMA
+            + COLUMN_TAKE_NUM + INTCOMMA
+            + COLUMN_RATING + INTCOMMA
+            + COLUMN_CHECKING + INTEGER
+            + " )";
+
     public static final String DELETE_ENTRIES = "DROP TABLE IF EXISTS " + ProjectEntry.TABLE_PROJECT;
+    public static final String DELETE_TAKES = "DROP TABLE IF EXISTS " + ProjectEntry.TABLE_TAKES;
 }
 
--- RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/ProjectManager/ProjectDatabaseHelper.java
+++ RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/ProjectManager/ProjectDatabaseHelper.java
@@ -9,12 +9,29 @@ import android.database.sqlite.SQLiteOpenHelper;
 import java.util.ArrayList;
 import java.util.List;
 
+import wycliffeassociates.recordingapp.FilesPage.FileNameExtractor;
+
 import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.TABLE_PROJECT;
 import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_TARGET_LANG;
 import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_SLUG;
 import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_SOURCE;
 import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_PROJECT;
 import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_MODE;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_SOURCE_LANG;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_BOOK_NUM;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_CONTRIBUTORS;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_SOURCE_AUDIO_PATH;
+
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.TABLE_TAKES;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_CHAPTER;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_START_VS;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_END_VS;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_RATING;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_CHECKING;
+import static wycliffeassociates.recordingapp.ProjectManager.ProjectContract.ProjectEntry.COLUMN_TAKE_NUM;
+
+
+
 
 /**
  * Created by sarabiaj on 5/10/2016.
@@ -58,10 +75,33 @@ public class ProjectDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DELETE FROM " + ProjectContract.ProjectEntry.TABLE_PROJECT);
     }
 
+    public String getProjectQuery(Project project){
+        String query = "SELECT * FROM " + ProjectContract.ProjectEntry.TABLE_PROJECT + " WHERE " +
+                ProjectContract.ProjectEntry.COLUMN_SLUG + " = '" + project.getSlug() + "' AND " +
+                ProjectContract.ProjectEntry.COLUMN_SOURCE + " = '" + project.getSource() + "' AND " +
+                ProjectContract.ProjectEntry.COLUMN_TARGET_LANG + " = '" + project.getTargetLanguage() + "' AND " +
+                ProjectContract.ProjectEntry.COLUMN_MODE + " = '" + project.getMode() + "' AND " +
+                ProjectContract.ProjectEntry.COLUMN_PROJECT + " = '" + project.getProject() + "'";
+        return query;
+    }
+
+    public String getTakeQuery(FileNameExtractor fne){
+        String query = "SELECT * FROM " + TABLE_TAKES + " WHERE " +
+                COLUMN_TARGET_LANG + " = '" + fne.getLang() + "' AND " +
+                COLUMN_SOURCE + " = '" + fne.getSource() + "' AND " +
+                COLUMN_SLUG + " = '" + fne.getBook() + "' AND " +
+                COLUMN_CHAPTER + " = " + fne.getChapter() + " AND " +
+                COLUMN_START_VS + " = " + fne.getStartVerse() + " AND " +
+                COLUMN_END_VS + " = " + fne.getEndVerse() + " AND " +
+                COLUMN_TAKE_NUM + " = " + fne.getTake() + "";
+        return query;
+    }
+
     public boolean deleteProject(Project p){
         SQLiteDatabase db = getWritableDatabase();
 
-        return db.delete(TABLE_PROJECT,
+        //Delete the project entry from the projects table as well as all individual files related from the takes table
+        boolean success = (db.delete(TABLE_PROJECT,
                 COLUMN_PROJECT + "=? AND " +
                 COLUMN_SLUG + "=? AND " +
                 COLUMN_TARGET_LANG + "=? AND " +
@@ -74,46 +114,97 @@ public class ProjectDatabaseHelper extends SQLiteOpenHelper {
                         p.getMode(),
                         p.getSource()
                 }
-        ) > 0;
+        ) > 0) && (db.delete(TABLE_PROJECT,
+                COLUMN_TARGET_LANG + "=? AND " +
+                COLUMN_SOURCE + "=? AND " +
+                COLUMN_SLUG + "=? AND " +
+                COLUMN_MODE + "=?",
+                new String[]{
+                        p.getTargetLanguage(),
+                        p.getSource(),
+                        p.getSlug(),
+                        p.getMode()
+                }
+        ) > 0);
+        db.close();
+        return success;
+    }
+
+    public void deleteTake(FileNameExtractor fne){
+        SQLiteDatabase db = getWritableDatabase();
+        String delete = "DELETE * FROM " + TABLE_TAKES + " WHERE " +
+                COLUMN_TARGET_LANG + " = '" + fne.getLang() + "' AND " +
+                COLUMN_SOURCE + " = '" + fne.getSource() + "' AND " +
+                COLUMN_SLUG + " = '" + fne.getBook() + "' AND " +
+                COLUMN_CHAPTER + " = " + fne.getChapter() + " AND " +
+                COLUMN_START_VS + " = " + fne.getStartVerse() + " AND " +
+                COLUMN_END_VS + " = " + fne.getEndVerse() + " AND " +
+                COLUMN_TAKE_NUM + " = " + fne.getTake() + "";
+        db.execSQL(delete);
+        db.close();
     }
 
     public void addProject(Project p){
         SQLiteDatabase db = getWritableDatabase();
         if(!checkIfExists(p)) {
             ContentValues cv = new ContentValues();
-            cv.put(ProjectContract.ProjectEntry.COLUMN_TARGET_LANG, p.getTargetLanguage());
-            cv.put(ProjectContract.ProjectEntry.COLUMN_SOURCE_LANG, p.getSourceLanguage());
-            cv.put(ProjectContract.ProjectEntry.COLUMN_SLUG, p.getSlug());
-            cv.put(ProjectContract.ProjectEntry.COLUMN_SOURCE, p.getSource());
-            cv.put(ProjectContract.ProjectEntry.COLUMN_MODE, p.getMode());
-            cv.put(ProjectContract.ProjectEntry.COLUMN_BOOK_NUM, p.getBookNumber());
-            cv.put(ProjectContract.ProjectEntry.COLUMN_PROJECT, p.getProject());
-            cv.put(ProjectContract.ProjectEntry.COLUMN_CONTRIBUTORS, p.getContributors());
-            cv.put(ProjectContract.ProjectEntry.COLUMN_SOURCE_AUDIO_PATH, p.getSourceAudioPath());
+            cv.put(COLUMN_TARGET_LANG, p.getTargetLanguage());
+            cv.put(COLUMN_SOURCE_LANG, p.getSourceLanguage());
+            cv.put(COLUMN_SLUG, p.getSlug());
+            cv.put(COLUMN_SOURCE, p.getSource());
+            cv.put(COLUMN_MODE, p.getMode());
+            cv.put(COLUMN_BOOK_NUM, p.getBookNumber());
+            cv.put(COLUMN_PROJECT, p.getProject());
+            cv.put(COLUMN_CONTRIBUTORS, p.getContributors());
+            cv.put(COLUMN_SOURCE_AUDIO_PATH, p.getSourceAudioPath());
             long result = db.insert(ProjectContract.ProjectEntry.TABLE_PROJECT, null, cv);
         }
         db.close();
     }
 
-    private void getIds(Cursor cursor){
-        mIdSlug = cursor.getColumnIndex(ProjectContract.ProjectEntry.COLUMN_SLUG);
-        mIdTargetLang = cursor.getColumnIndex(ProjectContract.ProjectEntry.COLUMN_TARGET_LANG);
-        mIdSourceLang = cursor.getColumnIndex(ProjectContract.ProjectEntry.COLUMN_SOURCE_LANG);
-        mIdSource = cursor.getColumnIndex(ProjectContract.ProjectEntry.COLUMN_SOURCE);
-        mIdMode = cursor.getColumnIndex(ProjectContract.ProjectEntry.COLUMN_MODE);
-        mIdBookNum = cursor.getColumnIndex(ProjectContract.ProjectEntry.COLUMN_BOOK_NUM);
-        mIdProject = cursor.getColumnIndex(ProjectContract.ProjectEntry.COLUMN_PROJECT);
-        mIdContributors = cursor.getColumnIndex(ProjectContract.ProjectEntry.COLUMN_CONTRIBUTORS);
-        mIdSourceAudioPath = cursor.getColumnIndex(ProjectContract.ProjectEntry.COLUMN_SOURCE_AUDIO_PATH);
+    public void addTake(FileNameExtractor fne, int rating, int checking){
+        SQLiteDatabase db = getWritableDatabase();
+        if(!checkIfExists(fne)){
+            ContentValues cv = new ContentValues();
+            cv.put(COLUMN_TARGET_LANG, fne.getLang());
+            cv.put(COLUMN_SOURCE, fne.getSource());
+            cv.put(COLUMN_SLUG, fne.getBook());
+            cv.put(COLUMN_CHAPTER, fne.getChapter());
+            cv.put(COLUMN_START_VS, fne.getStartVerse());
+            cv.put(COLUMN_END_VS, fne.getEndVerse());
+            cv.put(COLUMN_TAKE_NUM, fne.getTake());
+            cv.put(COLUMN_RATING, rating);
+            cv.put(COLUMN_CHECKING, checking);
+            long result = db.insert(TABLE_TAKES, null, cv);
+        }
+        db.close();
+    }
+
+    private void getProjectTableIds(Cursor cursor){
+        mIdSlug = cursor.getColumnIndex(COLUMN_SLUG);
+        mIdTargetLang = cursor.getColumnIndex(COLUMN_TARGET_LANG);
+        mIdSourceLang = cursor.getColumnIndex(COLUMN_SOURCE_LANG);
+        mIdSource = cursor.getColumnIndex(COLUMN_SOURCE);
+        mIdMode = cursor.getColumnIndex(COLUMN_MODE);
+        mIdBookNum = cursor.getColumnIndex(COLUMN_BOOK_NUM);
+        mIdProject = cursor.getColumnIndex(COLUMN_PROJECT);
+        mIdContributors = cursor.getColumnIndex(COLUMN_CONTRIBUTORS);
+        mIdSourceAudioPath = cursor.getColumnIndex(COLUMN_SOURCE_AUDIO_PATH);
     }
 
     public boolean checkIfExists(Project project){
-        String query = "SELECT * FROM " + ProjectContract.ProjectEntry.TABLE_PROJECT + " WHERE " +
-                ProjectContract.ProjectEntry.COLUMN_SLUG + " = '" + project.getSlug() + "' AND " +
-                ProjectContract.ProjectEntry.COLUMN_SOURCE + " = '" + project.getSource() + "' AND " +
-                ProjectContract.ProjectEntry.COLUMN_TARGET_LANG + " = '" + project.getTargetLanguage() + "' AND " +
-                ProjectContract.ProjectEntry.COLUMN_MODE + " = '" + project.getMode() + "' AND " +
-                ProjectContract.ProjectEntry.COLUMN_PROJECT + " = '" + project.getProject() + "'";
+        String query = getProjectQuery(project);
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        if(cursor.getCount() > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkIfExists(FileNameExtractor fne){
+        String query = getTakeQuery(fne);
 
         SQLiteDatabase db = getReadableDatabase();
         Cursor cursor = db.rawQuery(query, null);
@@ -129,7 +220,7 @@ public class ProjectDatabaseHelper extends SQLiteOpenHelper {
         String query = "SELECT * FROM " + ProjectContract.ProjectEntry.TABLE_PROJECT;
         SQLiteDatabase db = getReadableDatabase();
         Cursor cursor = db.rawQuery(query, null);
-        getIds(cursor);
+        getProjectTableIds(cursor);
 
         if(cursor.moveToFirst()){
             do {
@@ -159,5 +250,52 @@ public class ProjectDatabaseHelper extends SQLiteOpenHelper {
         cursor.close();
         db.close();
         return count;
+    }
+
+    private int getIntFromTakeTable(FileNameExtractor fne, String column){
+        String query = getTakeQuery(fne);
+        SQLiteDatabase db = getReadableDatabase();
+        db.execSQL(query);
+        Cursor cursor = db.rawQuery(query, null);
+        getProjectTableIds(cursor);
+        int val = 0;
+        if(cursor.moveToFirst()){
+            val = cursor.getInt(cursor.getColumnIndex(column));
+        }
+        cursor.close();
+        db.close();
+        return val;
+    }
+
+    private void setIntInTakeTable(FileNameExtractor fne, String column, int value){
+        String query = "UPDATE " + TABLE_TAKES + " SET " + column + "=" + value
+                + " WHERE " +
+                COLUMN_TARGET_LANG + " = '" + fne.getLang() + "' AND " +
+                COLUMN_SOURCE + " = '" + fne.getSource() + "' AND " +
+                COLUMN_SLUG + " = '" + fne.getBook() + "' AND " +
+                COLUMN_CHAPTER + " = " + fne.getChapter() + " AND " +
+                COLUMN_START_VS + " = " + fne.getStartVerse() + " AND " +
+                COLUMN_END_VS + " = " + fne.getEndVerse() + " AND " +
+                COLUMN_TAKE_NUM + " = " + fne.getTake() + "";
+
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(query);
+        db.close();
+    }
+
+    public int getRating(FileNameExtractor fne){
+        return getIntFromTakeTable(fne, COLUMN_RATING);
+    }
+
+    public int getCheckingLevel(FileNameExtractor fne){
+        return getIntFromTakeTable(fne, COLUMN_CHECKING);
+    }
+
+    public void setRating(FileNameExtractor fne, int rating){
+        setIntInTakeTable(fne, COLUMN_RATING, rating);
+    }
+
+    public void setCheckingLevel(FileNameExtractor fne, int checkingLevel){
+        setIntInTakeTable(fne, COLUMN_CHECKING, checkingLevel);
     }
 }
