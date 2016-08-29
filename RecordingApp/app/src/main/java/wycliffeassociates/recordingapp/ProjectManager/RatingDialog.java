a3c11b9a8479830e6dac120602b9ad777b44c56a
Vicky Leong <vleong2332@gmail.com>
2016-08-29 11:52:03 -0400

    Make rating modal pre-fill current rating

--- RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/ProjectManager/RatingDialog.java
+++ RecordingApp/app/src/main/java/wycliffeassociates/recordingapp/ProjectManager/RatingDialog.java
@@ -18,19 +18,24 @@ import wycliffeassociates.recordingapp.widgets.FourStepImageView;
 public class RatingDialog extends DialogFragment {
 
     public static String TAKE_KEY = "key_take_name";
+    public static String CURRENT_RATING_KEY = "key_current_rating";
+
     public interface DialogListener {
         void onPositiveClick(RatingDialog dialog);
         void onNegativeClick(RatingDialog dialog);
-
     }
+
     private int mRating;
     private DialogListener mListener;
     private String mTakeName;
 
-    public static RatingDialog newInstance(String takeName){
+    private FourStepImageView mOneStar, mTwoStar, mThreeStar;
+
+    public static RatingDialog newInstance(String takeName, int currentRating){
         RatingDialog rate = new RatingDialog();
         Bundle args = new Bundle();
         args.putString(TAKE_KEY, takeName);
+        args.putInt(CURRENT_RATING_KEY, currentRating);
         rate.setArguments(args);
         return rate;
     }
@@ -40,6 +45,7 @@ public class RatingDialog extends DialogFragment {
         super.onCreate(savedInstanceState);
         Bundle args = getArguments();
         mTakeName = args.getString(TAKE_KEY);
+        mRating = args.getInt(CURRENT_RATING_KEY);
     }
 
     @Override
@@ -65,39 +71,32 @@ public class RatingDialog extends DialogFragment {
             @Override
             public void onShow(DialogInterface dialog) {
                 AlertDialog alertDialog= (AlertDialog) dialog;
-                final FourStepImageView oneStar = (FourStepImageView) alertDialog.findViewById(R.id.one_star_rating);
-                final FourStepImageView twoStar = (FourStepImageView) alertDialog.findViewById(R.id.two_star_rating);
-                final FourStepImageView threeStar = (FourStepImageView) alertDialog.findViewById(R.id.three_star_rating);
+                mOneStar = (FourStepImageView) alertDialog.findViewById(R.id.one_star_rating);
+                mTwoStar = (FourStepImageView) alertDialog.findViewById(R.id.two_star_rating);
+                mThreeStar = (FourStepImageView) alertDialog.findViewById(R.id.three_star_rating);
 
-                oneStar.setOnClickListener(new View.OnClickListener() {
+                mOneStar.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        oneStar.setStep(1);
-                        twoStar.setStep(0);
-                        threeStar.setStep(0);
-                        mRating = 1;
+                        setRating(1);
                     }
                 });
 
-                twoStar.setOnClickListener(new View.OnClickListener() {
+                mTwoStar.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        oneStar.setStep(2);
-                        twoStar.setStep(2);
-                        threeStar.setStep(0);
-                        mRating = 2;
+                        setRating(2);
                     }
                 });
 
-                threeStar.setOnClickListener(new View.OnClickListener() {
+                mThreeStar.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        oneStar.setStep(3);
-                        twoStar.setStep(3);
-                        threeStar.setStep(3);
-                        mRating = 3;
+                        setRating(3);
                     }
                 });
+
+                setRating(mRating);
             }
         });
 
@@ -111,6 +110,31 @@ public class RatingDialog extends DialogFragment {
             mListener = (DialogListener) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString() + " must implement RatingDialogListener");
+        }
+    }
+
+    private void setRating(int rating) {
+        mRating = rating;
+        switch (mRating) {
+            case 1:
+                mOneStar.setStep(1);
+                mTwoStar.setStep(0);
+                mThreeStar.setStep(0);
+                break;
+            case 2:
+                mOneStar.setStep(2);
+                mTwoStar.setStep(2);
+                mThreeStar.setStep(0);
+                break;
+            case 3:
+                mOneStar.setStep(3);
+                mTwoStar.setStep(3);
+                mThreeStar.setStep(3);
+                break;
+            default:
+                mOneStar.setStep(0);
+                mTwoStar.setStep(0);
+                mThreeStar.setStep(0);
         }
     }
 
