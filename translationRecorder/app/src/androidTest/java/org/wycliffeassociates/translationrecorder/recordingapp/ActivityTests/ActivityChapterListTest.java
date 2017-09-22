b44da7b79730388fca3e7eb7584be2babc167494
Joe <jsarabia1247@gmail.com>
2017-09-22 16:31:41 -0400

    Changed to use espresso kinda (hacky)
    Hack explanation in comments...

--- translationRecorder/app/src/androidTest/java/org/wycliffeassociates/translationrecorder/recordingapp/ActivityTests/ActivityChapterListTest.java
+++ translationRecorder/app/src/androidTest/java/org/wycliffeassociates/translationrecorder/recordingapp/ActivityTests/ActivityChapterListTest.java
@@ -4,18 +4,22 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.RecyclerView;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wycliffeassociates.translationrecorder.ProjectManager.activities.ActivityChapterList;
 import org.wycliffeassociates.translationrecorder.ProjectManager.activities.ActivityUnitList;
 import org.wycliffeassociates.translationrecorder.ProjectManager.adapters.ChapterCardAdapter;
+import org.wycliffeassociates.translationrecorder.R;
 import org.wycliffeassociates.translationrecorder.TestUtils.FragmentTestActivity;
 import org.wycliffeassociates.translationrecorder.chunkplugin.Chapter;
 import org.wycliffeassociates.translationrecorder.chunkplugin.ChunkPlugin;
@@ -26,6 +30,9 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.List;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 import static org.wycliffeassociates.translationrecorder.recordingapp.ProjectMockingUtil.createBibleTestProject;
 import static org.wycliffeassociates.translationrecorder.recordingapp.ProjectMockingUtil.createNotesTestProject;
@@ -90,32 +97,31 @@ public class ActivityChapterListTest  {
         for(int i = 0; i < chapters.size(); i++) {
             final ChapterCard cc = ((ChapterCardAdapter)rv.getAdapter()).getItem(i);
             assertEquals(cc.getChapterNumber(), chapters.get(i).getNumber());
-            View v = rv.getChildAt(0);
-            final ChapterCardAdapter.ViewHolder ccvh = (ChapterCardAdapter.ViewHolder) rv.getChildViewHolder(v);
+            Instrumentation.ActivityMonitor activityMonitor = new Instrumentation.ActivityMonitor(ActivityUnitList.class.getName(), null, false);
+            InstrumentationRegistry.getInstrumentation().addMonitor(activityMonitor);
 
-            //the problem here is that binding view holders requires running on the ui thread
-            //binding the chapter card to the view holder seems to be the only way I can definitely
-            //access the chapter card and be able to click on it
-            chapterListActivity.runOnUiThread(new Runnable() {
-                                                  @Override
-                                                  public void run() {
-                                                      ccvh.bindViewHolder(ccvh, 0, cc);
-                                                  }
-                                              });
-                    //rv.getAdapter().bindViewHolder(ccvh, i);
+
+            //this hack seems to work? the sleep is necessary probably to give enough time for the data to bind to the view holder
+            //fumbling around first with the scrolling seems to be necessary for it to not throw an exception saying one of the chapter numbers can't match
+            onView(withId(R.id.chapter_list)).perform(RecyclerViewActions.scrollToHolder(withChapterNumber(cc.getChapterNumber())));
+            onView(withId(R.id.chapter_list)).perform(RecyclerViewActions.scrollToHolder(withChapterNumber(chapters.get(0).getNumber())));
+            onView(withId(R.id.chapter_list)).perform(RecyclerViewActions.scrollToHolder(withChapterNumber(cc.getChapterNumber())));
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            Instrumentation.ActivityMonitor activityMonitor = new Instrumentation.ActivityMonitor(ActivityUnitList.class.getName(), null, false);
-            InstrumentationRegistry.getInstrumentation().addMonitor(activityMonitor);
-            ccvh.onClick(ccvh.cardView);
+            onView(withId(R.id.chapter_list)).perform(RecyclerViewActions.actionOnHolderItem(withChapterNumber(cc.getChapterNumber()), click()));
+
+
             ActivityUnitList aul = (ActivityUnitList) activityMonitor.waitForActivity();
 
             Field chapterField = ActivityUnitList.class.getDeclaredField("mChapterNum");
             chapterField.setAccessible(true);
             int unitListChapter = chapterField.getInt(aul);
+            System.out.println("chapters.get is " + chapters.get(i).getNumber());
+            System.out.println("i is " + i);
+            System.out.println("unitListChapter is " + unitListChapter);
             assertEquals(unitListChapter, chapters.get(i).getNumber());
             System.out.println("SUCCESS: UnitListChapter " + unitListChapter + " and Chapter Number " + chapters.get(i).getNumber() + " are the same!");
             aul.finish();
@@ -124,6 +130,24 @@ public class ActivityChapterListTest  {
         chapterListActivity.finish();
         instrumentation.removeMonitor(chapterListMonitor);
 
+    }
+
+    public static Matcher<RecyclerView.ViewHolder> withChapterNumber(final int chapterNumber)
+    {
+        return new BoundedMatcher<RecyclerView.ViewHolder, ChapterCardAdapter.ViewHolder>(ChapterCardAdapter.ViewHolder.class)
+        {
+            @Override
+            protected boolean matchesSafely(ChapterCardAdapter.ViewHolder item)
+            {
+                return item.chapterCard.getChapterNumber() == chapterNumber;
+            }
+
+            @Override
+            public void describeTo(Description description)
+            {
+                description.appendText("view holder with chapter number: " + chapterNumber);
+            }
+        };
     }
 
 }
